84da74c1fdc800e70ecdc2d30064cb10b39ea096
jhonnierandrey <jhonnierandrey@outlook.com>
2021-06-08 22:42:25 -0300

    new footer added

--- src/App.tsx
+++ src/App.tsx
@@ -107,6 +107,9 @@ class App extends Component {
             </textarea>
           </div>
         </header>
+        <footer id="footer">
+			  	<p>&copy; 2021 <a href="https://www.jaesmadeit.com/" target="_blank" rel="noopener noreferrer">JAES Made It</a> | Made with <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> + <a href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">TypeScript</a> | <a href="https://github.com/jhonnierandrey/matrix" target="_blank" rel="noopener noreferrer">GitHub</a> | <a href="http://jhonnierandrey.info/" target="_blank" rel="noopener noreferrer">See more</a> | <a href="https://www.buymeacoffee.com/jhonnierandrey" target="_blank" rel="noopener noreferrer"><i className="fas fa-coffee"></i></a></p>
+			  </footer>
       </div>
     );
   }
